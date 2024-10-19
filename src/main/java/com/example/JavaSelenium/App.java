a0e8f6f0008c4d1aec9cd239b2dee17b1b560b37
Shankarrao Birajdar <100437902+ShankarraoBirajdar@users.noreply.github.com>
2024-10-19 23:28:47 +0530

    Revert "new changes"
    This reverts commit ab50c5274b15dfdd103a54c18e46975019c3b57b.

--- src/main/java/com/example/JavaSelenium/App.java
+++ src/main/java/com/example/JavaSelenium/App.java
@@ -2,31 +2,26 @@ package com.example.JavaSelenium;
 
 import java.util.Locale;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.manager.SeleniumManager;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class App {
 
+	
 	public static void main(String[] args) {
+		// driver path
+//		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
 		String os = System.getProperty("os.name", "generic").toLowerCase(Locale.ROOT);
 		System.out.println("OS Name: " + os);
-		
-//		storeSession();
-//		chromInDebuggingMode();
-//		chromeForTesting();
-//		selenium4();
+		selenium4();
 		webDriverManager();
 	}
-
+	
 	@Test
 	public void Test() {
 		String os = System.getProperty("os.name", "generic").toLowerCase(Locale.ROOT);
@@ -34,64 +29,25 @@ public class App {
 		selenium4();
 		webDriverManager();
 	}
-	
-	public static void storeSession() {
-		// add chrome options
-		ChromeOptions chromeOptions = new ChromeOptions();
-		chromeOptions.addArguments("--remote-allow-origins=*");
-//		chromeOptions.addArguments("--headless=new");
-		chromeOptions.addArguments("--start-maximized");
-		chromeOptions.addArguments("--disable-infobars");
-		chromeOptions.addArguments("--disable-notifications");
-		//chromeOptions.addArguments("--incognito");
-		chromeOptions.addArguments("--no-sandbox");
-		chromeOptions.addArguments("--user-data-dir=C:\\ChromeDebug");
-//        String chatLink = "https://web.whatsapp.com/send/?phone=" + "+917709667556" + "&text=" + "Hi";
-		String whatsappLink = "https://web.whatsapp.com/";
 
-		// create a driver
-		WebDriver driver = new ChromeDriver(chromeOptions);
-		driver.get(whatsappLink);
-		
-		
-		System.out.println("ChromeDriver launched successfully...");
-//		driver.quit();
-//		System.out.println("ChromeDriver closed successfully...");
-	}
-	
-	
-
-	
 	public static void selenium4() {
 		// add chrome options
 		System.out.println("Execution started");
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--remote-allow-origins=*");
-//		chromeOptions.addArguments("--headless=new");
+		chromeOptions.addArguments("--headless=new");
 		chromeOptions.addArguments("--start-maximized");
 		chromeOptions.addArguments("--disable-infobars");
 		chromeOptions.addArguments("--disable-notifications");
-//		chromeOptions.addArguments("--incognito");
-		chromeOptions.addArguments("--no-sandbox");
-		
-		chromeOptions.addArguments("user-agent=automation");
-		
-		
+
+		SeleniumManager seleniumManager = SeleniumManager.getInstance();
+		String pathString = seleniumManager.getDriverPath(chromeOptions);
+		System.out.println("getDriverPath: " + pathString);
+
 		// create a driver
 		WebDriver driver = new ChromeDriver(chromeOptions);
-		
-		Capabilities cap = ((RemoteWebDriver) driver).getCapabilities();
-	    String browserName = cap.getBrowserName().toLowerCase();
-	    System.out.println(browserName);
-	    String os = cap.getPlatformName().toString();
-	    System.out.println(os);
-	    String v = cap.getBrowserVersion().toString();
-	    System.out.println(v);
-		String url = "https://www.theaa.com/breakdown-cover/sales/cover";
-		
-		driver.get(url);
+		driver.get("https://google.com");
 		System.out.println("ChromeDriver launched successfully...");
-		System.out.println("Page Title:: "+driver.getTitle());
 		driver.quit();
 		System.out.println("ChromeDriver closed successfully...");
 	}
@@ -112,7 +68,7 @@ public class App {
 
 		// create a driver
 		WebDriver driver = WebDriverManager.chromedriver().capabilities(chromeOptions).create();
-		driver.get("https://web.whatsapp.com/");
+		driver.get("https://google.com");
 		System.out.println("ChromeDriver launched successfully...");
 		driver.quit();
 		System.out.println("ChromeDriver closed successfully...");
